c9035c0b306805088523d966963b563cbbd20fc1
Robocoders <team@blackbox.ai>
2024-10-15 00:35:12 +0000

    Update

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ app.use(express.json());
 
 const frangosCrusRouter = require('./routes/frangosCrus');
 const insumosRouter = require('./routes/insumos');
+const vendasRouter = require('./routes/vendas');
 const planejamentoProducaoRouter = require('./routes/planejamentoProducao');
 
 app.use('/api/frangos-crus', frangosCrusRouter);
